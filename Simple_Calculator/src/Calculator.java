221044bf6e31890f9b32a27635d621f7d3158b2f
USER <USER@test.com>
2018-03-30 11:50:58 +0900

    test1

--- Simple_Calculator/src/Calculator.java
+++ Simple_Calculator/src/Calculator.java
@@ -1,10 +1,9 @@
-// test1
-// test2
+
 public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Calculator is initialized...");
+		System.out.println("Test");
 	}
 
 }
